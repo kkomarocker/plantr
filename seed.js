4bb7994d5af88423da8db4bdc240893ac7ea5bd8
Jay.Lee <kkomarocker@gmail.com>
2018-04-30 23:13:20 -0400

    .then is properly working, except last one (See comment)

--- seed.js
+++ seed.js
@@ -1,23 +1,38 @@
 const db = require("./models");
 
-const carrot = db.models.vegetables.create(
-  {name: 'Carrot', color: 'Orange', planted_on: Date.now()}
-)
 
-const jay = carrot.then((vegetable) => {
-  console.log(db.models.vegetables.id)
-  return db.models.gardeners.create(
-    {name: 'Jay',
-    age: 34,
-    favoriteVegetableId: db.models.vegetables.id}
-  );
-});
-
-
+db.models.vegetables.create({
+  name: 'Carrot',
+  color: 'Orange',
+  planted_on: Date.now()
+})
+  .then((vegetables) => {
+    return db.models.gardeners.create(
+      { name: 'Jay',
+        age: 34,
+        favoriteVegetableId: vegetables.id
+      }
+    );
+  })
+  .then((gardeners) => {
+    return db.models.plots.create(
+      { size: 2,
+        shaded: true,
+        gardenerId: gardeners.id
+      }
+    )
+  })
+  // .then((vegetable_plot) => {
+  //   db.models.vegetable_plot.create(
+  //     {
+        // How can I access to return values of previous promises that was passed on above .then((vegetables)) & .then((gardeners))?
+  //     }
+  //   );
+  // });
 
 db.sync()
   .then(() => {
-    console.log("DB is synced!")
+    console.log("DB is synced!");
   }).catch((error) => {
     console.log(error);
   })
